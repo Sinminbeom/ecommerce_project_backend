6b2618714deda01add91694f1ad3634ba18b054f
mbshin <mbshin@swm.ai>
2023-12-10 23:11:24 +0900

    feat: #3 Response 구현
    close #3

---
+++ src/main/java/com/minbeom/EcommerceProjectBackend/response/Response.java
@@ -0,0 +1,18 @@
+package com.minbeom.EcommerceProjectBackend.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter @Getter
+public class Response {
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private int status;
+    private Object data;
+
+    public Response(int status, Object data) {
+        this.status = status;
+        this.data = data;
+    }
+}
